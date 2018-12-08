0bf5688c3dcc2c69581afab75d86633ef8be6f24
yury.tveritin <yury.tveritin@ardecs.ru>
2018-12-08 14:35:34 +0400

    develop

--- src/test/java/com/ardecs/SpringDataJpaJava/com/ardecs/SpringDataJpaJavaTests.java
+++ src/test/java/com/ardecs/SpringDataJpaJava/com/ardecs/SpringDataJpaJavaTests.java
@@ -21,7 +21,6 @@ import com.ardecs.SpringDataJpaJava.Repository.ProductRepository;
 import com.ardecs.SpringDataJpaJava.Repository.ReportRepository;
 import com.ardecs.SpringDataJpaJava.config.ConfigTest;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,7 +31,6 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static com.ardecs.SpringDataJpaJava.Repository.specification.ClientSpecificaton.clientFindByCriteries;
-import static com.ardecs.SpringDataJpaJava.Repository.specification.ProductSpecificaton.productFindByName;
 import static junit.framework.TestCase.assertEquals;
 import static org.junit.Assert.assertArrayEquals;
 
