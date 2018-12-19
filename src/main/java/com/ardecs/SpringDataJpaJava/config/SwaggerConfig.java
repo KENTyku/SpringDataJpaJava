f0ff0027c1b09f8fb30e1f21ef190d55069541d8
yury.tveritin <yury.tveritin@ardecs.ru>
2018-12-19 17:05:09 +0400

    dev

--- src/main/java/com/ardecs/SpringDataJpaJava/config/SwaggerConfig.java
+++ src/main/java/com/ardecs/SpringDataJpaJava/config/SwaggerConfig.java
@@ -1,11 +1,25 @@
 package com.ardecs.SpringDataJpaJava.config;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import com.ardecs.SpringDataJpaJava.controller.restful.CountryRESTFulController;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -20,16 +34,37 @@ public class SwaggerConfig extends WebMvcConfigurerAdapter {
 //указываем обработчик jar файлов для swagger
         registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
-
+    @ApiResponses(value = {
+            @ApiResponse(code=400, message = "This is a bad request, please stick to the API description"),
+            @ApiResponse(code=401, message = "Your request cannot be authorized.")
+    })
     @Bean
     public Docket api() {
+               List<ResponseMessage> responseMessages = Arrays.asList(
+                new ResponseMessageBuilder().code(400).message("Bad Request").build(),
+                new ResponseMessageBuilder().code(403).message("Forbidden").build(),
+                new ResponseMessageBuilder().code(404).message("NotFound").build(),
+                new ResponseMessageBuilder().code(401).message("Unauthorized").build()
+                );
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.ardecs.SpringDataJpaJava.controller.restful"))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .useDefaultResponseMessages(false)
+                .globalResponseMessage(RequestMethod.PUT,responseMessages)
+                ;
+
     }
 
-
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "My REST API",
+                "Some custom description of API.",
+                "API TOS",
+                "Terms of service",
+                new Contact("John Doe", "www.example.com", "myeaddress@company.com"),
+                "License of API", "API license URL", Collections.emptyList());
+    }
 }
 
