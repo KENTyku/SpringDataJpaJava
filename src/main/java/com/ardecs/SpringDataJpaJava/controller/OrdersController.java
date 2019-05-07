48722256580d8f2bce8227893a9b66884ef9d41e
yury.tveritin <yury.tveritin@ardecs.ru>
2019-05-07 10:56:07 +0400

    Добавлен подсчет стоимости заказа в разделе Список заказов

--- src/main/java/com/ardecs/SpringDataJpaJava/controller/OrdersController.java
+++ src/main/java/com/ardecs/SpringDataJpaJava/controller/OrdersController.java
@@ -1,16 +1,21 @@
 package com.ardecs.SpringDataJpaJava.controller;
 
-import com.ardecs.SpringDataJpaJava.Entity.Client;
-import com.ardecs.SpringDataJpaJava.Entity.Order;
-import com.ardecs.SpringDataJpaJava.Repository.ClientRepository;
-import com.ardecs.SpringDataJpaJava.Repository.OrderRepository;
+import java.util.HashMap;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import java.util.List;
+import com.ardecs.SpringDataJpaJava.Entity.Client;
+import com.ardecs.SpringDataJpaJava.Entity.Order;
+import com.ardecs.SpringDataJpaJava.Entity.OrderPosition;
+import com.ardecs.SpringDataJpaJava.Repository.ClientRepository;
+import com.ardecs.SpringDataJpaJava.Repository.OrderRepository;
 
 @Controller
 public class OrdersController {
@@ -21,9 +26,21 @@ public class OrdersController {
 
     @RequestMapping(value = {"/orders"}, method = RequestMethod.GET)
     public String showOrders(Model model) {
-        Client client = clientRepository.findByName("Yuri");
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String clientLogin = user.getUsername();
+        Client client = clientRepository.findByLogin(clientLogin);
         List<Order> orderList = orderRepository.findAllByClientOrderByDateDesc(client);
         model.addAttribute(orderList);
+        HashMap ordersMap = new HashMap();
+        float coastOrder = 0;
+        for (Order order : orderList) {
+            for (OrderPosition position : order.getOrderPositions()) {
+                coastOrder = coastOrder + position.getId().getProduct().getPrice() * position.getQuantity();
+            }
+            ordersMap.put(order, coastOrder);
+
+        }
+        model.addAttribute("ordersMap", ordersMap);
         return "orders";
     }
 }
