fc74bc5976e70decfe6e2c2a7201e608ed48984a
yury.tveritin <yury.tveritin@ardecs.ru>
2019-06-03 18:14:23 +0400

    Edit controllers for bootstrap template: add HomeController for home page, edit ProductsController.java

--- src/main/java/com/ardecs/SpringDataJpaJava/controller/HomeController.java
+++ src/main/java/com/ardecs/SpringDataJpaJava/controller/HomeController.java
@@ -3,23 +3,24 @@ package com.ardecs.SpringDataJpaJava.controller;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.ardecs.SpringDataJpaJava.Entity.Product;
-import com.ardecs.SpringDataJpaJava.Repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.ardecs.SpringDataJpaJava.Entity.Product;
+import com.ardecs.SpringDataJpaJava.Repository.ProductRepository;
+
 @Controller
-public class ProductsController {
+public class HomeController {
     @Autowired
     private ProductRepository productRepository;
 
-    @RequestMapping(value = {"/product"}, method = RequestMethod.GET)
+    @RequestMapping(value = {"/", "/home"}, method = RequestMethod.GET)
     public String showProducts(Model model) {
         List<Product> productsList = (ArrayList<Product>) productRepository.findAll();
         model.addAttribute(productsList);
-        return "products";
+        return "index";
     }
 }
