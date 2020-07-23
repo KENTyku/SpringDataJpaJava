f277e435312da5a076c9923202f9ff2815ae6f71
yury <kentyku@bk.ru>
2020-07-23 14:49:04 +0400

    Migrate without JPA

--- src/main/java/com/ardecs/SpringDataJpaJava/SpringDataJpaJava.java
+++ src/main/java/com/ardecs/SpringDataJpaJava/SpringDataJpaJava.java
@@ -1,12 +1,14 @@
 package com.ardecs.SpringDataJpaJava;
 
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@Controller
-//@SpringBootApplication
-//@ContextConfiguration(locations = {"classpath:META-INF/app-context.xml"})//указываем конфиг для работы автокофигурации контекста в тесте
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+//Чтобы исключить источник данных и гибернацию JPA auto-configuration, используйте:
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class })
 public class SpringDataJpaJava {
     public static void main(String[] args) {
+        SpringApplication.run(SpringDataJpaJava.class, args);
     }
 }
